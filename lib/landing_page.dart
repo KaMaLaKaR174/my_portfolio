86fb35f4d72504c5344fccaffcc5db31ed9f5a02
KaMaLaKaR174 <mucharlakamalakar@gmail.com>
2020-09-25 13:47:06 +0530

    Adjustments.

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -40,7 +40,7 @@ class LandingPage extends StatelessWidget {
             Text("An Android developer with decent programming skills seeking opportunities to work in a challenging environment and utilize my skills in achieving organizational goals.",
               textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.grey,fontStyle: FontStyle.italic,fontSize: 18,fontWeight: FontWeight.w200,
+                color: Colors.grey,fontStyle: FontStyle.italic,fontSize: 18,fontWeight: FontWeight.w300,
               ),
             ),
             SizedBox(height: 12,),
@@ -56,37 +56,38 @@ class LandingPage extends StatelessWidget {
 //                      _launchEmail("mucharlakamalakar@gmail.com","Through Portfolio","Hey!! Want to connect with you!");
 //                    },
 //                    child: Image.asset("assets/gmail.png",height: 30,width: 30,)),
+              //https://www.facebook.com/kamalakar.mucharla
 //                SizedBox(width: 12,),
                 GestureDetector(
                     onTap: (){
                       _launchURL("https://github.com/KaMaLaKaR174");
                     },
 
-                    child: Image.asset("assets/github.png",height: 20,width: 20,)),
+                    child: Image.asset("assets/github.png",height: 22,width: 22,)),
                 SizedBox(width: 12,),
                 GestureDetector(
                     onTap: (){
                       _launchURL("https://www.linkedin.com/in/kamalakar-mucharla/");
                     },
-                    child: Image.asset("assets/linkedin.png",height: 20,width: 20,)),
+                    child: Image.asset("assets/linkedin.jpg",height: 24,width: 24,)),
                 SizedBox(width: 12,),
                 GestureDetector(
                   onTap: (){
                     _launchURL("https://twitter.com/Kamalak19364854?s=08");
                   },
-                    child: Image.asset("assets/twitter.png",height: 20,width: 20,)),
+                    child: Image.asset("assets/twitter.png",height: 22,width: 22,)),
                 SizedBox(width: 12,),
                 GestureDetector(
                     onTap: (){
                       _launchURL("https://join.skype.com/invite/WCxQY4DZvCid");
                     },
-                    child: Image.asset("assets/skype.png",height: 20,width: 20,)),
+                    child: Image.asset("assets/skype.png",height: 24,width: 24,)),
                 SizedBox(width: 12,),
                 GestureDetector(
                   onTap: (){
                     _launchURL("https://www.instagram.com/kamalakar_mucharla_/?hl=en");
                   },
-                  child: Image.asset("assets/instagram.png",height: 20,width: 20,
+                  child: Image.asset("assets/instagram.png",height: 22,width: 22,
                   ),
                 ),
                 SizedBox(width: 12,),
